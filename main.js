ca5efa890e7522bd109b66f6cf34f10407994a5c
saljaoui <soufian2022ff@gmail.com>
2025-05-30 19:00:06 +0100

    update

---
+++ main.js
@@ -0,0 +1,21 @@
+    const loader = document.getElementById('loader');
+    const bar = document.querySelector('.loader-bar');
+    const percent = document.querySelector('.loader-percent');
+
+    // Custom progress steps
+    const steps = [10, 50, 75, 100];
+    let index = 0;
+
+    const interval = setInterval(() => {
+        const value = steps[index];
+        bar.style.width = value + '%';
+        percent.textContent = value + '%';
+
+        index++;
+
+        if (index >= steps.length) {
+            clearInterval(interval);
+            loader.style.opacity = '0';
+            setTimeout(() => loader.style.display = 'none', 500);
+        }
+    }, 500);
