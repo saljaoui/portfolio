04e12b6f301a49aa495dada98a83db5d29cfc1e6
saljaoui <soufian2022ff@gmail.com>
2025-11-14 19:59:25 +0100

    start new portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import './App.css'
 
 function App() {
+
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
+     <div className="p-10">
+      <h1 className="text-3xl font-bold text-red-500">
+        Tailwind Test
+      </h1>
+
+      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+        Button Test
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
